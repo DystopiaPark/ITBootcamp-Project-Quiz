3f0f3bd74ab24bbe89124bbb6620e5041bc522f6
Filip <68464364+DystopiaPark@users.noreply.github.com>
2023-01-06 12:41:52 +0100

    update

--- js/filozofija.js
+++ js/filozofija.js
@@ -81,20 +81,14 @@ var rezultatiSection = document.getElementById("rezultati-section");
 function shuffle(array) {
   let currentIndex = array.length,
     randomIndex;
-
-  // While there remain elements to shuffle.
   while (currentIndex != 0) {
-    // Pick a remaining element.
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex--;
-
-    // And swap it with the current element.
     [array[currentIndex], array[randomIndex]] = [
       array[randomIndex],
       array[currentIndex],
     ];
   }
-
   return array;
 }
 shuffle(nizFilozofija);
